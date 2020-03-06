f4dcbd38ba49cab8aa6cd4e02c16c3ed5bfded1d
jonathanbun <jbunnell@rams.colostate.edu>
2020-03-05 20:56:46 -0700

    ...

--- huffman.py
+++ huffman.py
@@ -1,89 +1,140 @@
 import sys
 import math
-import datetime
+
+array_length = 0 # ONCE SET FOR A GIVEN ARRAY, THIS SHOULD NEVER CHANGE
+# EVEN WHEN DOING OPERATIONS LIKE EXTRACT-MIN AND HEAP-INSERT
+
+n = 0 #length of the 'heap'. n <= array_length
 
 
-class Heap:
-    def __init__(self, A):
-        self.A = A
-        self.length = len(A)
-        self.n = self.length
 
-    def swap(self, i, j):
-        self.A[i], self.A[j] = self.A[j], self.A[i]
+def swap(A, i, j):
+    A[i], A[j] = A[j], A[i]
 
-    def parent(self, i):
-        return (i - 1) // 2
+def parent(i):
+    return (i - 1) // 2
 
-    def left(self, i):
-        return 2 * i + 1
+def left(i):
+    return 2 * i + 1
 
-    def right(self, i):
-        return self.left(i) + 1
+def right(i):
+    return left(i) + 1
 
-    def heapify(self, i):
-        if self.left(i) >= self.n and self.right(i) >= self.n:
-            return
-        elif self.left(i) < self.n <= self.right(i):
+def heapify(A, i):
+    if left(i) >= n and right(i) >= n:
+        return
+    elif left(i) < n <= right(i):
 
-            if self.A[i] == self.A[self.left(i)] or self.A[i] < self.A[self.left(i)]:
+        if A[i] > A[left(i)]:
 
-                self.swap(i, self.left(i))
-                self.heapify(self.left(i))
-            else:
-                return
+            swap(A, i, left(i))
+            heapify(A, left(i))
         else:
-            if self.A[self.left(i)] < self.A[self.right(i)]:
-                self.swap(i, self.left(i))
-                self.heapify(self.left(i))
-            else:
-                self.swap(i, self.right(i))
-                self.heapify(self.right(i))
-
-    def buildHeap(self):
-        middleOfList = self.n // 2
-        for i in range(middleOfList, -1, -1):
-            self.heapify(i)
-
-    def heapExtractMin(self):
-        if self.A[0] is None:
             return
-        if self.n < 1:
-            return
+    elif A[i] > A[left(i)] or A[i] > A[right(i)]:
 
-        minElement = self.A[0]
-        self.swap(0, self.n - 1)
-        self.A[self.n - 1] = None
-        self.n -= 1
 
-        self.heapify(0)
+        if A[left(i)] < A[right(i)]:
+            swap(A, i, left(i))
+            heapify(A, left(i))
+        else:
+            swap(A, i, right(i))
+            heapify(A, right(i))
 
-        return minElement
+def buildHeap(A):
+    global n
+    n = len(A)  # The entire list A will be turned into a heap.
+    # So length of heap = length of array
+    middleOfList = (n // 2)
 
-    def heapInsert(self, v):
+    for i in range(middleOfList, -1, -1):
+        heapify(A, i)
 
-        if self.n >= self.length:
-            return
-        self.A[self.n] = v
-        temp = self.n
-        self.n += 1
+def heapExtractMin(A):
+    global n
+    if A[0] is None:
+        return
+    if n < 1:
+        return
 
-        if (self.A[self.parent(temp)]) is not None:
-            while (self.A[self.parent(temp)] < v and self.parent(temp) > 0):
-                print(self.A[self.parent(temp)])
-                self.swap(temp, self.parent(temp))
-                temp = self.parent(temp)
-                if self.A[self.parent(temp)] is None:
-                    break
+    minElement = A[0]
+    swap(A, 0, n - 1)
+    A[n - 1] = None
+    n -= 1
 
+    heapify(A, 0)
+
+    return minElement
+
+def heapInsert(A, v):
+    global n
+    if n >= len(A):
+        return
+    A[n] = v
+    temp = n
+    n += 1
+
+    if (A[parent(temp)]) is not None:
+
+
+
+        while A[parent(temp)] > v and parent(temp) >= 0:
+            swap(A, temp, parent(temp))
+            temp = parent(temp)
+            if A[parent(temp)] is None:
+                break
+
+
+class PriorityTuple(tuple):
+    """A specialization of tuple that compares only its first item when sorting.
+    Create one using double parens e.g. PriorityTuple((x, (y, z))) """
+
+    def __lt__(self, other):
+
+        return self[0] < other[0]
+
+    def __le__(self, other):
+        return self[0] <= other[0]
+
+    def __gt__(self, other):
+
+        return self[0] > other[0]
+
+    def __ge__(self, other):
+        return self[0] >= other[0]
+
+    def __eq__(self, other):
+        return self[0] == other[0]
+
+    def __ne__(self, other):
+        x = self.__eq__(other)
+        return not x
+
+
+def left(i):
+    return (2 * i) + 1
+
+
+def right(i):
+    return left(i) + 1
+
+
+def buildCodes(abc, freqs, total=""):
+    a = str(abc)
+    value = 0
+    for char in a.split():
+        if char in freqs:
+            freqs[char] = "{0:b}".format(value)
+            value += 1
 
 
 def file_character_frequencies(file_name):
-    freqs = {}
+    freqs = {' ': 0, '\n': 0}
     numOfChars = 0
     with open(file_name) as file:
         for line in file:
             fields = line.split()
+            nl = line.split('\n')
             for word in fields:
                 for letter in word:
                     if letter in freqs:
@@ -91,41 +142,48 @@ def file_character_frequencies(file_name):
                     else:
                         freqs[letter] = 1
                     numOfChars += 1
+        if len(nl) > 1:
+            freqs['\n'] += 1
+            numOfChars += 1
+        freqs[' '] += len(fields) - 1
+        numOfChars += len(fields) - 1
+
     for key in freqs:
         freqs[key] /= numOfChars
     return freqs
 
 
-class PriorityTuple(tuple):
-    """A specialization of tuple that compares only its first item when sorting.
-    Create one using double parens e.g. PriorityTuple((x, (y, z))) """
-    def __lt__(self, other):
-        return self[0] < other[0]
-
-    def __le__(self, other):
-        return self[0] <= other[0]
-
-
 def huffman_codes_from_frequencies(frequencies):
     # Suggested helper
+    global n
     huffmanArray = []
     for key in frequencies:
-        temp = PriorityTuple((frequencies[key], key))
+        temp = [PriorityTuple((frequencies[key], key))]
         huffmanArray.append(temp)
-    priorityQueue = Heap(huffmanArray)
-    priorityQueue.buildHeap()
-    while priorityQueue.n > 1:
-        temp1 = priorityQueue.heapExtractMin()
-        if temp1 is None:
-            break
-        temp2 = priorityQueue.heapExtractMin()
-        if temp2 is None:
 
-            break
-        newNode = PriorityTuple((temp1[0] + temp2[0], (temp1, temp2)))
+    buildHeap(huffmanArray)
+    total = 1
 
-        priorityQueue.heapInsert(newNode)
-    print(priorityQueue.A)
+
+    while n != 1:
+        temp1 = heapExtractMin(huffmanArray)
+
+        temp2 = heapExtractMin(huffmanArray)
+
+        temp5 = temp1[0][0] + temp2[0][0]
+        temp4 = [PriorityTuple((temp5, None)), temp1, temp2]
+
+
+
+
+        heapInsert(huffmanArray, temp4)
+        total += 1
+    foo(huffmanArray[0])
+    return frequencies
+
+def foo(A):
+    for x in A:
+        print(x)
 
 
 
@@ -168,34 +226,6 @@ def decode_file_using_codes(file_name_encoded, letter_codes):
                 partial_code = ""
     print("Wrote decoded text to {}".format(file_name_encoded_decoded))
 
-def shuffled_list(length, seed):
-    lst = list(range(10, length + 10))
-    import random
-    r = random.Random(seed) # pseudo random, so it is repeatable
-    r.shuffle(lst)
-    return lst
-
-def printCompleteTree(A):
-    """ A handy function provided to you, so you can see a
-    complete tree in its proper shape.
-    Note that this function shows the ENTIRE ARRAY in the form of a tree
-    """
-
-    height = int(math.log(len(A), 2))
-    width = len(str(max(A)))
-    for i in range(height + 1):
-        print(width * (2 ** (height - i) - 1) * " ", end="")
-        for j in range(2 ** i):
-            idx = 2 ** i - 1 + j
-            if idx >= len(A):
-                print()
-                break
-            if j == 2 ** i - 1:
-                print("{:^{width}}".format(A[idx], width=width))
-            else:
-                print("{:^{width}}".format(A[idx], width=width),
-                      width * (2 ** (height - i + 1) - 1) * " ", sep='', end='')
-    print()
 
 
 
@@ -212,8 +242,6 @@ def main():
     h.buildHeap()
     printCompleteTree(h.A)
     """
-
-
 
 
 if __name__ == '__main__':
